bc635f873464a7572ee6fef3c5ea5371b090bfba
Ricardo Yamamoto Abe <ricardoy@gmail.com>
2019-03-07 13:38:30 -0300

    Check if the queue is full before adding new files to be processed.

--- ep1/grep.c
+++ ep1/grep.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <errno.h>
 #include <regex.h>
 #include <dirent.h>
@@ -26,6 +27,9 @@ void grep_read_dir(char *path, a_list list) {
     while ((dir_entry = readdir(dir)) != NULL) {
         sprintf(buffer, "%s/%s", path, dir_entry->d_name);
         if (dir_entry->d_type == 8) {
+            while(a_list_is_full(list)) {
+                usleep(1);
+            }
             a_list_stradd(list, buffer);
         } else if (dir_entry->d_type == 4 && accept_dir(dir_entry->d_name)) {
             grep_read_dir(buffer, list);
